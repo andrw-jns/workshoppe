1297a6a3fb2edf09398e545a605935ba0b248361
andrw-jns <andrewghjones@gmail.com>
2017-12-12 08:51:10 +0000

    CCG data and script update.

--- r/cqc_eda.R
+++ r/cqc_eda.R
@@ -7,6 +7,7 @@
 library(janitor)
 library(readxl)
 library(GGally)
+library(stringr)
 library(tidyverse)
 
 # cqc_dir <- read_excel("./data/01 October 2015 HSCA Active Locations.xlsx", 
@@ -27,8 +28,8 @@ library(tidyverse)
 # cqc_dir <- cqc_dir %>% filter(care_home == 1)
 # 
 # saveRDS(cqc_dir, "./data/cqc_dir_ch.RDS")
-cqc_dir <- read_rds("./data/cqc_dir_ch.RDS")
-
+# cqc_dir <- read_rds("./data/cqc_dir_ch.RDS")
+# file.choose()
 # janitor::get_dupes(cqc_dir %>% select(-location_id))
 # No duplicates
 
@@ -44,8 +45,8 @@ cqc_dir <- read_rds("./data/cqc_dir_ch.RDS")
 # 
 # pairs_plot <- ggpairs(cqc_sample, columns = c(33, 51, 52, 81, 84))
 
-cqc_big <- cqc_dir[c(2, 3, 4, 8:18, 21, 22, 33, 81, 84)] %>%
-  mutate(id = row_number())
+# cqc_big <- cqc_dir[c(2, 3, 4, 8:18, 21, 22, 33, 81, 84)] %>%
+#   mutate(id = row_number())
 
 # Assumption 1:
 # Just Service user band (SUB) older people or SUB dementia. Will mean
@@ -64,11 +65,11 @@ cqc_big <- cqc_dir[c(2, 3, 4, 8:18, 21, 22, 33, 81, 84)] %>%
 
 # This is more like expected. 87% of beds for older people SUB.
 
-cqc_small <- cqc_big %>% 
-  filter(service_user_band_older_people == 1) %>% 
-  select(location_name,
-         care_homes_beds,
-         postal_code)
+# cqc_small <- cqc_big %>% 
+#   filter(service_user_band_older_people == 1) %>% 
+#   select(location_name,
+#          care_homes_beds,
+#          postal_code)
 
 
 # (1.1) POPS LSOA OVER 75 ------------------------------------------
@@ -106,8 +107,10 @@ lsoa_pop <- read_rds("./data/lsoa_pop_o75.RDS")
 # 
 # hes_grouped <- hes_ip_o75 %>% group_by(LSOA) %>% summarise(admissions = n())
 
-saveRDS(hes_grouped, "./data/hes.RDS")
+# saveRDS(hes_grouped, "./data/hes.RDS")
 hes <- read_rds("./data/hes.RDS")
+
+hes <- hes %>% filter(grepl("E*", LSOA))?
 
 # ggplot(hes_ip_o75, aes(admimeth))+
 #   geom_bar()
@@ -147,8 +150,6 @@ hes <- read_rds("./data/hes.RDS")
 # cqc_big2 <-  bind_rows(cqc_with_lsoa1,cqc_with_lsoa2) 
 # 
 # saveRDS(cqc_big2, "./data/cqc_with_lsoa.RDS")
-cqc_big <- read_rds("./data/cqc_with_lsoa.RDS")
-"Note missing entries"
 
 # newer postcodes?
 # missings <- anti_join(cqc_big, cqc_big2, by = c("id"))
@@ -162,30 +163,84 @@ cqc_big <- read_rds("./data/cqc_with_lsoa.RDS")
 
 # deprivation -------------------------------------------------------------
 
-deprivation <- read_csv("./data/societal-wellbeing-imd-indices.csv" , skip = 6)
-deprivation <- deprivation[-1]
+# deprivation <- read_csv("./data/societal-wellbeing-imd-indices.csv" , skip = 6)
+# deprivation <- deprivation[-1]
+# 
+# deprivation <- deprivation %>%
+#   separate(`Reference area` , c("Name", "LSOA") , sep = -10)
+# 
+# ## Check all indices in regression  model ##
+# deprivation <- deprivation[-1]
+# colnames(deprivation) <- c("lsoa", "imd", "income", "employment", "education",
+#                                     "health" , "crime", "services", "environment",
+#                                     "children",  "idaopi")
+# 
+# # See: English_Indices_of_Deprivation_2015_-_Statistical_Release.pdf
+# 
+# 
+# # Quintile reduces model complexity and shouldn't lose anything.
+# deprivation <- deprivation %>% 
+#   mutate(idaopi_quint = case_when(
+#     deprivation$idaopi %in% c(1,2)  ~ 1,
+#     deprivation$idaopi %in% c(3,4)  ~ 2,
+#     deprivation$idaopi %in% c(5,6)  ~ 3,
+#     deprivation$idaopi %in% c(7,8)  ~ 4,
+#     deprivation$idaopi %in% c(9,10) ~ 5
+#   ))
 
-deprivation <- deprivation %>%
-  separate(`Reference area` , c("Name", "LSOA") , sep = -10)
-
-## Check all indices in regression  model ##
-deprivation <- deprivation[-1]
-colnames(deprivation) <- c("lsoa", "imd", "income", "employment", "education",
-                                    "health" , "crime", "services", "environment",
-                                    "children",  "idaopi")
-
-# See: English_Indices_of_Deprivation_2015_-_Statistical_Release.pdf
+# saveRDS(deprivation, "./data/deprivation.rds")
 
 
-# Quintile reduces model complexity and shouldn't lose anything.
-deprivation <- deprivation %>% 
-  mutate(idaopi_quint = case_when(
-    deprivation$idaopi %in% c(1,2)  ~ 1,
-    deprivation$idaopi %in% c(3,4)  ~ 2,
-    deprivation$idaopi %in% c(5,6)  ~ 3,
-    deprivation$idaopi %in% c(7,8)  ~ 4,
-    deprivation$idaopi %in% c(9,10) ~ 5
-  ))
+# ccg ---------------------------------------------------------------------
 
-saveRDS(deprivation, "./data/deprivation.rds")
+# from the ONS open geography portal:
+lsoa_to_ccg <- read_csv("./data/Lower_Layer_Super_Output_Area_2011_to_Clinical_Commissioning_Group_to_Local_Authority_District_April_2016_Lookup_in_England.csv")
+lsoa_to_ccg <- lsoa_to_ccg[-c(2,3,6:8)]
+colnames(lsoa_to_ccg) <- c("lsoa", "ccg_code", "ccg_name")
+
+saveRDS(lsoa_to_ccg, "./data/ccg_lsoa.rds")
+
+
+# quick load individual -------------------------------------------------------------
+
+
+hes      <- read_rds("./data/hes.RDS") %>% filter(str_detect(LSOA, "E"))
+lsoa_pop <- read_rds("./data/lsoa_pop_o75.RDS")
+cqc_big  <- read_rds("./data/cqc_with_lsoa.RDS")
+"Note missing entries"
+
+ccg_lsoa    <- read_rds("./data/ccg_lsoa.rds")
+deprivation <- read_rds("./data/deprivation.rds") %>% select(lsoa, imd, idaopi, idaopi_quint)
+"deaths data"
+"cqc ratings"
+
+
+# joins -------------------------------------------------------------------
+
+map(list(lsoa_pop, hes, ccg_lsoa, cqc_big, deprivation), names)
+
+# the straightforward joins:
+join1 <- left_join(lsoa_pop, ccg_lsoa, by = c("area_code" = "lsoa"))
+join2 <- left_join(join1, deprivation, by = c("area_code" = "lsoa"))
+
+
+join3 <- full_join(join2, hes, by = c("area_code" = "LSOA"))
+# examine anti joins
+sum(is.na(join3$admissions)) # that's ok!!! 53 LSOAs don't have and admission
+# but there are no admissions that don't have an LSOA
+
+cqc_grouped <- cqc_big %>%
+  group_by(LSOA11CD) %>%
+  summarise(beds = sum(care_homes_beds))
+
+join4 <- full_join(join3, cqc_grouped, by = c("area_code" = "LSOA11CD"))
+# examine anti-joins, but that is it for now.
+
+dummy <- join4 %>% mutate(deaths = round(rnorm(32844, over_75/10, over_75/20)),
+                          ranking = round(runif(32844, 1, 5))) %>% 
+  mutate(deaths = if_else(deaths < 0 , 0, deaths))
+
+dummy_data <- saveRDS(dummy, "./data/carehome_dummy.rds")
+
+care_homes <- read_rds("./data/carehome_dummy.rds")
 
