f4a0935001978b8a5408049bdb51d113c0dc18dc
Andrew Jones <Andrew.Jones@clients.its.local>
2017-12-20 15:09:18 +0000

    Further caret inspired learning.

--- r/modelling_principles_01.R
+++ r/modelling_principles_01.R
@@ -16,9 +16,10 @@ care_homes <- read_rds(here("data", "carehome_dummy.rds"))
 but at some stage this will have to be looked at properly."
 
 care_homes <- care_homes %>% 
+  # NAs set as zero.
   mutate_at(vars(beds, admissions), funs(ifelse(is.na(.), 0, .))) %>% 
   # leave as factors and see what happens for now:
-  mutate_at(vars(ccg_name, idaopi_quint), funs("factor"))
+  mutate_at(vars(ccg_name, idaopi_quint, imd), funs("factor"))
 
 
 # Simple implementation of caret train -------------------------------
@@ -35,36 +36,61 @@ test_data <- test_data %>%
          beds
          )
 
+full_data <- care_homes %>% 
+  select(over_75,
+         ccg_name,
+         imd,
+         idaopi_quint,
+         admissions,
+         beds
+  )
+
 str(test_data)
+str(full_data)
 
-#Converting every categorical variable to numerical using dummy variables
-dmy <- dummyVars(" ~. ", data = test_data, fullRank = T)
-train_transformed <- data.frame(predict(dmy, newdata = test_data))
+#Converting every categorical variable to numerical using dummy variables:
+# dmy <- dummyVars(" ~. ", data = test_data, fullRank = T)
+# train_transformed <- data.frame(predict(dmy, newdata = test_data))
 
 
-samp <- createDataPartition(test_data$admissions, p = 0.8, list = FALSE)
-training <- test_data[samp,]
-testing  <- test_data[-samp,]
+samp <- createDataPartition(full_data$admissions, p = 0.8, list = FALSE)
+training <- full_data[samp,]
+testing  <- full_data[-samp,]
 
 # training <- as.data.frame(training)
 # class(training)
 
-xnam <- paste("x", 1:25, sep="")
-fmla <- as.formula(paste("y ~ ", paste(xnam, collapse= "+")))
+# Can produce the formula as a string:
+# xnam <- paste("x", 1:25, sep="")
+# fmla <- as.formula(paste("y ~ ", paste(xnam, collapse= "+")))
 
 "need to remove over_75 from the model"
 "May be better to do trainControl and preProcessing outside the regression" 
 
-nb1 <- train(admissions ~ offset(log(over_75)) + .,
+"If centre and scale the outcome (and the over_75) this shouldn't affect the outcome"
+"Check this by looking at quartiles of rates?"
+
+pre_proc <-  preProcess(select(training,
+                               #-ccg_name,
+                               -admissions,
+                               -over_75),
+                        method = c("center","scale"))
+
+pre_proc$method
+
+train_processed <- predict(pre_proc, training)
+
+"Research: Principal Component Analysis (pca) and when and whether it's useful"
+
+nb1 <- train(admissions ~ beds + idaopi_quint + ccg_name + offset(log(over_75)),
       # train(admissions ~ beds + idaopi_quint + ccg_name + offset(log(over_75)),
              # data       = train_transformed,
-             data = train_transformed,
-             preProcess = c("center", "scale"),
-             # in order to pre-process need to do dummy transform.
-             # and have a dataframe rather than tibble.
-             trControl  = trainControl(number = 5),
-             method     = "glm.nb",
-             tuneGrid   = data.frame(link = "log"))
+             data = train_processed, # is now the pre_processed data
+             # preProcess = c("center","scale"),
+             trControl  = trainControl(number = 8), # number of resamples
+             tuneGrid   = data.frame(link = c("log", "sqrt")),
+             method     = "glm.nb"
+      ) # specify 1 parameter (not sqrt or identity)
 
 "In parameters need to specify: link = log "
 " The rank deficiency is caused by ccg_name: 211 levels."
@@ -72,7 +98,29 @@ nb1 <- train(admissions ~ offset(log(over_75)) + .,
 # center and scaling results in a higher model rsquared.
 # But does the low Rsquared actually result in a useless model.
 
+# Model R^2 and McFadden's Pseudo R^2:
+# http://thestatsgeek.com/2014/02/08/r-squared-in-logistic-regression/
+
 nb1
+summary(nb1$finalModel$residuals) 
+# you could test fit of sqrt vs. log link by comparing plot of residuals.
+# or fitted vs. actual.
+
+
+https://stats.stackexchange.com/questions/46418/why-is-the-square-root-transformation-recommended-for-count-data
+"You should evaluate R-squared values in conjunction with residual plots, other model statistics, and subject area knowledge in order to round out the picture (pardon the pun)."
+"Graph the predictions against original for log and sqrt link functions"
+
+broom::tidy(nb1$finalModel)
+broom::glance(nb1$finalModel)
+rsquare(nb1$finalModel, train_processed)
+class(nb1$finalModel)
+# predict the outcome on a test set
+test_pred <- predict(nb1, train_processed)
+# compare predicted outcome and true outcome
+confusionMatrix(test_pred, train_processed$admissions)
+
+
 
 "https://stats.stackexchange.com/questions/35071/what-is-rank-deficiency-and-how-to-deal-with-it
 "
